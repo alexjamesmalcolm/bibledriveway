d206407753ad3161a50058279a937c9444450f2d
Alexander Malcolm <alexjamesmalcolm@gmail.com>
2023-11-01 19:24:08 -0400

    Compressing build output

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,26 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import XMLLoader from "./xml-loader";
-
 import nodejs from "@astrojs/node";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), compress()],
   output: "hybrid",
-  server: { host: "0.0.0.0" },
-  adapter: nodejs({ mode: "standalone" }),
-  vite: { plugins: [XMLLoader({ ignoreAttributes: false })] },
+  server: {
+    host: "0.0.0.0",
+  },
+  adapter: nodejs({
+    mode: "standalone",
+  }),
+  vite: {
+    plugins: [
+      XMLLoader({
+        ignoreAttributes: false,
+      }),
+    ],
+  },
 });
